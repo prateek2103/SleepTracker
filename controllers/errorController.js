d3b72f07fa377b208385201445c87fd2ca53032b
prateek2103 <prateek007.purohit@gmail.com>
2024-05-16 02:59:48 +0530

    structure issues fixed

--- controllers/errorController.js
+++
@@ -1,7 +0,0 @@
-exports.getNotFound = (req, res, next) => {
-  res.status(404).send("invalid request");
-};
-
-exports.getServerError = (err, req, res, next) => {
-  res.status(500).send("Something went wrong" + err);
-};
--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const userController = require("../controllers/userController");
-const { signupValidations } = require("../validations/userValidations");
-
-// POST /user/signup
-router.post("/signup", signupValidations, userController.postSignup);
-
-module.exports = router;
